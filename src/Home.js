1ceaaec66165e1688e5e0bf35afbf9bb993636fa
wesley <truthotwo@gmail.com>
2019-08-13 22:23:25 +0900

    loading movie data
    loading movie data

--- src/Home.js
+++ src/Home.js
@@ -1,5 +1,21 @@
 import React from "react";
+import { Query } from "react-apollo";
+import { HOME_PAGE } from "./queries";
 
-const Home = () => <div>Home</div>;
+const Home = () => (
+  <Query query={HOME_PAGE}>
+    {({ loading, data, error }) => {
+      if (loading) return <span>loading...</span>;
+      if (error) return <span>You got a problem!</span>;
+      if (data) {
+        return data.movies.map(movie => (
+          <h2 key={movie.id}>
+            {movie.title} / {movie.genres}
+          </h2>
+        ));
+      }
+    }}
+  </Query>
+);
 
 export default Home;
